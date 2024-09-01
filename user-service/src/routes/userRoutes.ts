7993502c88067471900f094db1222e6452b75998
mintsai7777 <king840110@gmail.com>
2024-09-01 23:29:15 +0800

    add check token api

--- user-service/src/routes/userRoutes.ts
+++ user-service/src/routes/userRoutes.ts
@@ -31,7 +31,7 @@ router.post('/login', async (req, res) => {
   try {
     const user = await userRepository.findOne({ where: { username } });
 
-    if (user && await bcrypt.compare(password, user.password)) {
+    if (user && (await bcrypt.compare(password, user.password))) {
       const token = generateToken(user.id);
       res.status(200).json({ message: 'Login successful', token });
     } else {
@@ -44,16 +44,22 @@ router.post('/login', async (req, res) => {
 
 router.get('/profile', authMiddleware, async (req: UserRequest, res) => {
   try {
-    const user = await userRepository.findOne({ where: { id: req.user?.id }, select: ['username', 'email'] });
+    const user = await userRepository.findOne({
+      where: { id: req.user?.id },
+      select: ['username', 'email'],
+    });
     if (user) {
       res.status(200).json(user);
     } else {
-      res.status(404).json({ error: 'user not found' })
+      res.status(404).json({ error: 'user not found' });
     }
   } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch user profile' })
+    res.status(500).json({ error: 'Failed to fetch user profile' });
   }
-})
+});
 
+router.get('/checkToken', authMiddleware, async (req, res) => {
+  res.status(200).json({ message: 'Token is valid' });
+});
 
 export default router;
