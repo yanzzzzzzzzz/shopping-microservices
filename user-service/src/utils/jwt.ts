96b94e85987dda2c59f9ca3585d2370fbd8604a3
yanzzzzzzzzz <king840110@gmail.ccom>
2024-10-25 17:33:37 +0800

    token expired time

--- user-service/src/utils/jwt.ts
+++ user-service/src/utils/jwt.ts
@@ -6,7 +6,7 @@ dotenv.config();
 const SECRET_KEY = process.env.SECRET_KEY || 'default-secret-key';
 
 export const generateToken = (user: User) => {
-  return jwt.sign({ id: user.id, username: user.username }, SECRET_KEY, { expiresIn: '1h' });
+  return jwt.sign({ id: user.id, username: user.username }, SECRET_KEY, { expiresIn: '3h' });
 };
 
 export const verifyToken = (token: string): { id: number } | null => {
